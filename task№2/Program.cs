328413f107bf1d402d1020bb3a4f3902aafb1461
Valentina-Moon <valentina.tirtik_777@mail.ru>
2022-10-23 15:07:56 +0300

    числа  Фибоначчи

---
+++ task№2/Program.cs
@@ -0,0 +1,15 @@
+﻿//Показать числа Фибоначчи.
+
+double Fibonacci(int n)
+{
+    if (n == 1 || n == 2) return 1;
+    else return Fibonacci(n - 1) + Fibonacci(n - 2);
+}
+    Console.WriteLine("Значение элемента Фибоначчи: ");
+    int n = int.Parse(Console.ReadLine() ?? "0");
+    Console.WriteLine();
+
+for (int i = 1; i < n; i++)
+{
+    Console.WriteLine($"f({i}) = {Fibonacci(i)}");
+}
